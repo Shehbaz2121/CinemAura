246738f13e0eb487d2b6215dc996b4f79088a354
Shehbaz Ali <shehbaz42069@gmail.com>
2026-05-29 04:36:46 +0500

    edited login pages password logic for admin

--- auth/login.php
+++ auth/login.php
@@ -24,8 +24,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
                 $_SESSION['user_id']   = $user['id'];
                 $_SESSION['user_name'] = $user['full_name'];
                 $_SESSION['user_role'] = $user['role'];
-
-                header('Location: ../pages/home.php');
+              if($_SESSION['user_role'] === 'admin'){
+              header('Location: ../admin/index.php');
+               } else {
+               header('Location: ../pages/home.php');
+               }
                 exit();
             } else {
                 $error = "Invalid email or password.";
