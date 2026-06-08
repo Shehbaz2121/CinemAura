cb3ed8bec953614dcc8db0cd099691384a4ade5b
Shehbaz <shehbaz42069@gmail.com>
2026-06-08 10:22:27 +0500

    Fixed Header

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,28 @@
-
+<?php
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <link rel="stylesheet" href="../assets/css/homenavfoot.css">
 <nav>
   <a href="../index.php" class="logo">Cinem<span>Aura</span></a>
   <ul class="nav-links">
     <li><a href="movies.php">Movies</a></li>
-    <li><a href="#">Theaters</a></li>
     <li><a href="showtimes.php">Showtimes</a></li>
-    <li><a href="#">Reviews</a></li>
+    <li><a href="booking.php">Book Tickets</a></li>
   </ul>
   <div class="nav-actions">
-    <?php if(isset($_SESSION['user_id'])): ?>
-          <a href="#">Welcome, <?= $_SESSION['user_name'] ?></a>
-          <a href="../auth/logout.php" class="btn btn-gold">Logout</a>
-        <?php if($_SESSION['user_role'] === 'admin'): ?>
-          <a href="../admin/index.php" class="btn btn-gold">Dashboard</a>
-          <?php endif; ?>
-          <?php else: ?>
-        <a href="../auth/login.php" class="btn btn-outline">Sign In</a>
-        <a href="../auth/register.php" class="btn btn-gold">Register</a>
-
+    <?php if (isset($_SESSION['user_id'])): ?>
+      <?php if (isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin'): ?>
+        <a href="../admin/index.php" class="btn btn-outline">Dashboard</a>
+      <?php endif; ?>
+      <a href="../auth/logout.php" class="btn btn-gold">Logout</a>
+    <?php else: ?>
+      <a href="../auth/login.php" class="btn btn-outline">Sign In</a>
+      <a href="../auth/register.php" class="btn btn-gold">Register</a>
     <?php endif; ?>
-</div>
+  </div>
+  
+  </button>
 </nav>
